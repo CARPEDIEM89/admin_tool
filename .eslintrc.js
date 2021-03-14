4b1a99979d6be306f5216213616e658f426bfab6
CARPEDIEM89 <38743683+CARPEDIEM89@users.noreply.github.com>
2021-03-14 15:39:02 +0900

    Initial commit!

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb', // or airbnb-base
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // 설치 한경우
+    'plugin:import/errors', // 설치한 경우
+    'plugin:import/warnings', // 설치한 경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    'prettier/prettier': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+   ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
